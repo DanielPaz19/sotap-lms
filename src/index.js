fa722ef0d9664e4f8ff73c5d239818f955c96410
DanielPaz19 <anyepaz1019@gmail.com>
2022-09-10 00:08:25 +0800

    react route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/index.css";
-import App from "./App";
+import Dashboard from "./pages/Dashboard";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />}>
+          <Route index path="dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../../components/Nav";
+import Main from "../../components/Main";
+
+function Dashboard() {
+  return (
+    <>
+      <Nav />
+      <Main />
+    </>
+  );
+}
+
+export default Dashboard;
