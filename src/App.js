50b3407353e2c770c45786e76782d4ea090d0ade
DanielPaz19 <anyepaz1019@gmail.com>
2022-09-09 23:47:19 +0800

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import React from "react";
 import Nav from "./components/Nav";
+import Main from "./components/Main";
 
 function App() {
-  return <Nav />;
+  return (
+    <>
+      <Nav />
+      <Main />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Main/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+import Header from "../Header";
+import Content from "../Content";
+
+function Main() {
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  );
+}
+
+export default Main;
