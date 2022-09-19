626d3752435667ac12b534789991ebdae148c313
DanielPaz19 <anyepaz1019@gmail.com>
2022-09-19 18:42:37 +0800

    react player

--- src/pages/Topics/index.js
+++ src/pages/Topics/index.js
@@ -1,6 +1,7 @@
 import BreadCrumb from "../../components/Breadcrumb";
 import useGetSubjectById from "../../customHooks/useGetSubjectById";
 import useGetTopicById from "../../customHooks/useGetTopicById";
+import ReactPlayer from "react-player";
 
 function Topics() {
   const topic = useGetTopicById();
@@ -16,6 +17,13 @@ function Topics() {
     <>
       <BreadCrumb paths={path} />
       <h1>{topic?.title}</h1>
+      <div className="container">
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=cWExc9Y6sLA"
+          controls={true}
+          width={"100%"}
+        />
+      </div>
     </>
   );
 }
