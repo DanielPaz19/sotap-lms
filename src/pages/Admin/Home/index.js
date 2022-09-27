7f33d95531f2069561657d6ddd4577691fe244d7
DanielPaz19 <anyepaz1019@gmail.com>
2022-09-27 18:34:31 +0800

    admin nav-items

--- src/pages/Admin/Home/index.js
+++ src/pages/Admin/Home/index.js
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 import useLogInStatus from "../../../customHooks/useLoginStatus";
 import { ADMIN_USER } from "../../../config";
 import { useState } from "react";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Nav } from "react-bootstrap";
 import "./style.css";
 import { FiMenu } from "react-icons/fi";
 import { IoExitOutline } from "react-icons/io5";
@@ -35,9 +35,25 @@ function AdminHome() {
 
   return (
     <Container fluid className="px-0">
-      <div
-        className={`${navOpen ? "active" : ""} bg-primary admin--sidebar `}
-      ></div>
+      <div className={`${navOpen ? "active" : ""} bg-primary admin--sidebar `}>
+        <Nav activeKey="#" className="flex-column mt-5">
+          <Nav.Item>
+            <Nav.Link href="#" className="link-light">
+              Students
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1 " className="link-light">
+              Teachers
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-2" className="link-light">
+              Subjects
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </div>
 
       <div className="bg-light admin--content">
         <div className="admin--header d-md-flex justify-content-between ">
