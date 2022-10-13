74791cd19088d1c0f7bfa89dec5e9b24a0713b43
DanielPaz19 <anyepaz1019@gmail.com>
2022-10-14 00:05:50 +0800

    Add Student To Grade Levels

--- src/pages/Admin/GradeLevelDetails/index.js
+++ src/pages/Admin/GradeLevelDetails/index.js
@@ -1,4 +1,5 @@
-import { Button } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
 import { BsPlusLg } from "react-icons/bs";
 import { useParams } from "react-router-dom";
 import AdminStudentTable from "../../../components/AdminStudentTable";
@@ -6,22 +7,143 @@ import useAdmin from "../../../context/AdminContextProvider";
 
 function GradeLevelDetails() {
   const { id } = useParams();
-  const { state } = useAdmin();
+  const { state, addStudentToGrade } = useAdmin();
   const [grade_level] = state?.grade_levels.filter(
     (grade) => grade.id === Number(id)
   );
 
-  console.log(grade_level.students);
+  const [error, setError] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [formData, setFormData] = useState({
+    grade_id: id,
+    student_id: [],
+  });
+
+  // close Modal
+  const handleClose = () => {
+    setShowModal(false);
+    setError(false);
+  };
+
+  // Open Modal
+  const handleShow = () => setShowModal(true);
+
+  const submitData = async () => {
+    if (!formData.student_id.length) return setError(true);
+
+    await addStudentToGrade(formData);
+
+    // Hide modal
+    setShowModal(false);
+
+    // Reset FormData Default value
+    setFormData({
+      grade_id: id,
+      student_id: [],
+    });
+  };
+
+  const handleCheckBox = (e) => {
+    setError(false);
+    const checked = [];
+
+    // Loop through all input checks
+    document
+      .querySelectorAll("#checkStudent")
+      .forEach((item) => item.checked && checked.push(Number(item.value)));
+
+    setFormData((prev) => ({ ...prev, student_id: checked }));
+  };
 
   return (
     <>
       <div className="d-md-flex justify-content-between align-items-center mt-5">
         <h4 className="fw-bolder text-primary">{grade_level?.name}</h4>;
-        <Button variant="success">
+        <Button variant="success" onClick={handleShow}>
           <BsPlusLg /> Add Students
         </Button>
       </div>
-      <AdminStudentTable students={grade_level.students}/>
+      <AdminStudentTable students={grade_level?.students} />
+
+      <Modal
+        show={showModal}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        size="xl"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add Student</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form
+            onSubmit={(e) => {
+              e.preventDefault();
+              submitData();
+            }}
+          >
+            <AdminStudentTable
+              students={state?.students.filter(
+                (student) =>
+                  !grade_level?.students
+                    .map((item) => item.id)
+                    .includes(student.id)
+              )}
+              checkbox={true}
+              handleCheckBox={handleCheckBox}
+              error={error}
+            />
+            {/* 
+            <Form.Group className="mb-3">
+              <Form.Label>Level</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Level"
+                defaultValue={grade_level?.name}
+                disabled
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Name"
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    name: e.target.value,
+                  }))
+                }
+              />
+            </Form.Group> 
+          */}
+            <div className="d-md-flex justify-content-end">
+              {error ? (
+                <span className="text-danger me-5">
+                  Select atleast one (1) student to Proceed!
+                </span>
+              ) : (
+                ""
+              )}
+              <Button
+                variant="danger"
+                onClick={handleClose}
+                disabled={state?.loading ? true : false}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="primary"
+                type="submit"
+                className="ms-2"
+                disabled={state?.loading ? true : false}
+              >
+                Add to {grade_level?.name}
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
