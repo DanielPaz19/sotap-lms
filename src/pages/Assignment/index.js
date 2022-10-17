236b2daf67b46874bb619f1e627c29b50a017801
Simon <elordesimon6@outlook.com>
2022-10-17 14:58:14 +0800

    quizes page

---
+++ src/pages/Assignment/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+function Assignment() {
+    return ( 
+        <h2>Assignments</h2>
+        
+        
+        
+     );
+}
+
+export default Assignment;
--- src/pages/Quizes/index.js
+++ src/pages/Quizes/index.js
@@ -1,5 +1,58 @@
-function Quizes() {
-  return <h1>Quizes Page</h1>;
+import './style.css';
+// function Quizes() {
+//     return ( 
+//         <h2>Quizes</h2>
+        
+//      );
+     
+// }
+
+// export default Quizes;
+
+
+// import AccordionWrapper from './components/AccordionWrapper';
+// import AccordionItem from './components/AccordionItem';
+
+import AccordionWrapper from "../../components/AccordionWrapper";
+import AccordionItem from "../../components/AccordionItem";
+
+
+function Quizzes() {
+
+  const data = [
+    {
+      "title": "Completed Quizzes",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a consequat nibh. Mauris suscipit arcu at fermentum convallis. Pellentesque consectetur mi in felis maximus posuere."
+    },
+    {
+      "title": "Pending Quizzes",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie tellus a maximus tempus. Duis vel leo iaculis, porttitor erat et, posuere erat. Ut blandit."
+    },
+    {
+      "title": "Test",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia, nibh imperdiet tempus pharetra, arcu risus aliquet arcu, a auctor ex lacus efficitur purus. Morbi."
+    },
+    {
+      "title": "Testing",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat lobortis nibh, eu molestie est placerat non. Donec ornare nisl erat, non imperdiet elit porta."
+    }
+  ];
+
+  return (
+    <div className="App">
+      <div className="content">
+        <div className="app-description">
+          <h1>Quizzes</h1>
+          <p>View your Completed and Pending Quizzes</p>
+        </div>
+          <AccordionWrapper>
+            {data.map((item, index) => (
+              <AccordionItem key={index} index={index} title={item.title} description={item.description} />
+            ))}
+          </AccordionWrapper>
+      </div>
+    </div>
+  );
 }
 
-export default Quizes;
+export default Quizzes;
