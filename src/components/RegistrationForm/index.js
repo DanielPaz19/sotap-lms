8d8baebad42a306e366834549892b5ce08d95e0f
DanielPaz19 <anyepaz1019@gmail.com>
2022-10-20 02:16:57 +0800

    form validation

--- src/components/RegistrationForm/index.js
+++ src/components/RegistrationForm/index.js
@@ -1,36 +1,89 @@
+import { useCallback } from "react";
+import { useEffect } from "react";
 import { useState } from "react";
-import { Button, FloatingLabel, Form } from "react-bootstrap";
+import { Button, FloatingLabel, Form, Spinner } from "react-bootstrap";
 import { FaKey, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { API_URL } from "../../config";
 
 function RegistrationForm({ onSubmit }) {
-  const [formData, setFormData] = useState(null);
+  const [formData, setFormData] = useState({
+    student_id: 0,
+    firstname: "",
+    lastname: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    setError("");
+    const res = await fetch(API_URL + `/register/student`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await res.json();
+
+    if (res.status === 422) setError(data.message);
+
+    setIsLoading(false);
+  };
 
   return (
     <div className="col-md-5 col-12 position-relative">
       <div className="login mx-auto position-absolute top-50 start-50 translate-middle">
         <div className="text-center">
           <h2 className="loginTxt">Register</h2>
-          <Form className="px-4">
+          <Form
+            className="px-4"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+              console.log(formData);
+            }}
+          >
             <FloatingLabel label="User ID" className="mb-2 text-secondary">
               <Form.Control
-                type="text"
+                isInvalid={error ? true : false}
+                type="number"
                 placeholder="User ID"
                 autoComplete="off"
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    student_id: e.target.value,
+                  }))
+                }
               />
             </FloatingLabel>
             <FloatingLabel label="First Name" className="mb-2 text-secondary">
               <Form.Control
+                isInvalid={error ? true : false}
                 type="text"
-                placeholder="User ID"
+                placeholder="First Name"
                 autoComplete="off"
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    firstname: e.target.value,
+                  }))
+                }
               />
             </FloatingLabel>
             <FloatingLabel label="Last Name" className="mb-2 text-secondary">
               <Form.Control
+                isInvalid={error ? true : false}
                 type="text"
-                placeholder="User ID"
+                placeholder="Last Name"
                 autoComplete="off"
+                onChange={(e) =>
+                  setFormData((prev) => ({ ...prev, lastname: e.target.value }))
+                }
               />
             </FloatingLabel>
 
@@ -40,12 +93,23 @@ function RegistrationForm({ onSubmit }) {
               size="lg"
               className="w-100 mt-3"
             >
-              Register
+              {isLoading ? (
+                <Spinner
+                  as="span"
+                  animation="border"
+                  size="sm"
+                  role="status"
+                  aria-hidden="true"
+                />
+              ) : (
+                "Next >"
+              )}
             </Button>
           </Form>
           <div className="mt-2 mb-5">
             <Link to="/login">Login</Link>
           </div>
+          <div className="text-danger mb-3">{error ? error : ""}</div>
         </div>
       </div>
     </div>
