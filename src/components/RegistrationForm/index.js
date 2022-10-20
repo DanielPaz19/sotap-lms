321f4040f2bd1bdb0c77b3dc20cc3c8cec70eadc
DanielPaz19 <anyepaz1019@gmail.com>
2022-10-21 02:25:08 +0800

    student and teacher account registration

--- src/components/RegistrationForm/index.js
+++ src/components/RegistrationForm/index.js
@@ -1,37 +1,42 @@
-import { useCallback } from "react";
-import { useEffect } from "react";
 import { useState } from "react";
 import { Button, FloatingLabel, Form, Spinner } from "react-bootstrap";
-import { FaKey, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { API_URL } from "../../config";
 
 function RegistrationForm({ onSubmit }) {
   const [formData, setFormData] = useState({
-    student_id: 0,
+    role: 0,
+    user_id: 0,
     firstname: "",
     lastname: "",
+    username: "",
+    password: "",
   });
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+  const [userFound, setUserFound] = useState(false);
 
   const handleSubmit = async () => {
     setIsLoading(true);
     setError("");
-    const res = await fetch(API_URL + `/register/student`, {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
+    const res = await fetch(
+      API_URL + `/register/${formData.role === 2 ? "teacher" : "student"}`,
+      {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+    );
 
     const data = await res.json();
-
-    if (res.status === 422) setError(data.message);
-
     setIsLoading(false);
+
+    if (res.status === 422) return setError(data.message);
+
+    setUserFound(true);
   };
 
   return (
@@ -47,64 +52,148 @@ function RegistrationForm({ onSubmit }) {
               console.log(formData);
             }}
           >
-            <FloatingLabel label="User ID" className="mb-2 text-secondary">
-              <Form.Control
-                isInvalid={error ? true : false}
-                type="number"
-                placeholder="User ID"
-                autoComplete="off"
-                onChange={(e) =>
-                  setFormData((prev) => ({
-                    ...prev,
-                    student_id: e.target.value,
-                  }))
-                }
-              />
-            </FloatingLabel>
-            <FloatingLabel label="First Name" className="mb-2 text-secondary">
-              <Form.Control
-                isInvalid={error ? true : false}
-                type="text"
-                placeholder="First Name"
-                autoComplete="off"
-                onChange={(e) =>
-                  setFormData((prev) => ({
-                    ...prev,
-                    firstname: e.target.value,
-                  }))
-                }
-              />
-            </FloatingLabel>
-            <FloatingLabel label="Last Name" className="mb-2 text-secondary">
-              <Form.Control
-                isInvalid={error ? true : false}
-                type="text"
-                placeholder="Last Name"
-                autoComplete="off"
-                onChange={(e) =>
-                  setFormData((prev) => ({ ...prev, lastname: e.target.value }))
-                }
-              />
-            </FloatingLabel>
+            {userFound ? (
+              <>
+                <FloatingLabel label="Username" className="mb-2 text-secondary">
+                  <Form.Control
+                    value={formData.username}
+                    isInvalid={error ? true : false}
+                    type="text"
+                    placeholder="Username"
+                    autoComplete="off"
+                    onChange={(e) =>
+                      setFormData((prev) => ({
+                        ...prev,
+                        username: e.target.value,
+                      }))
+                    }
+                  />
+                </FloatingLabel>
 
-            <Button
-              variant="primary"
-              type="submit"
-              size="lg"
-              className="w-100 mt-3"
-            >
-              {isLoading ? (
-                <Spinner
-                  as="span"
-                  animation="border"
-                  size="sm"
-                  role="status"
-                  aria-hidden="true"
-                />
-              ) : (
-                "Next >"
-              )}
-            </Button>
+                <FloatingLabel label="Password" className="mb-2 text-secondary">
+                  <Form.Control
+                    value={formData.password}
+                    isInvalid={error ? true : false}
+                    type="password"
+                    placeholder="Password"
+                    autoComplete="off"
+                    onChange={(e) =>
+                      setFormData((prev) => ({
+                        ...prev,
+                        password: e.target.value,
+                      }))
+                    }
+                  />
+                </FloatingLabel>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  size="lg"
+                  className="w-100 mt-3"
+                >
+                  {isLoading ? (
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    "Register"
+                  )}
+                </Button>
+              </>
+            ) : (
+              <>
+                <FloatingLabel
+                  label="Register as"
+                  className="mb-2 text-secondary"
+                >
+                  <Form.Select
+                    required
+                    defaultValue=""
+                    onChange={(e) =>
+                      setFormData((prev) => ({
+                        ...prev,
+                        role: Number(e.target.value),
+                      }))
+                    }
+                  >
+                    <option value="">Select one ...</option>
+                    <option value="2">Teacher</option>
+                    <option value="3">Student</option>
+                  </Form.Select>
+                </FloatingLabel>
+
+                <FloatingLabel label="User ID" className="mb-2 text-secondary">
+                  <Form.Control
+                    isInvalid={error ? true : false}
+                    type="number"
+                    placeholder="User ID"
+                    autoComplete="off"
+                    onChange={(e) =>
+                      setFormData((prev) => ({
+                        ...prev,
+                        user_id: e.target.value,
+                      }))
+                    }
+                  />
+                </FloatingLabel>
+                <FloatingLabel
+                  label="First Name"
+                  className="mb-2 text-secondary"
+                >
+                  <Form.Control
+                    isInvalid={error ? true : false}
+                    type="text"
+                    placeholder="First Name"
+                    autoComplete="off"
+                    onChange={(e) =>
+                      setFormData((prev) => ({
+                        ...prev,
+                        firstname: e.target.value,
+                      }))
+                    }
+                  />
+                </FloatingLabel>
+                <FloatingLabel
+                  label="Last Name"
+                  className="mb-2 text-secondary"
+                >
+                  <Form.Control
+                    isInvalid={error ? true : false}
+                    type="text"
+                    placeholder="Last Name"
+                    autoComplete="off"
+                    onChange={(e) =>
+                      setFormData((prev) => ({
+                        ...prev,
+                        lastname: e.target.value,
+                      }))
+                    }
+                  />
+                </FloatingLabel>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  size="lg"
+                  className="w-100 mt-3"
+                >
+                  {isLoading ? (
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    "Next >"
+                  )}
+                </Button>
+              </>
+            )}
           </Form>
           <div className="mt-2 mb-5">
             <Link to="/login">Login</Link>
