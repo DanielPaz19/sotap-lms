60b93fc85b35bc635fad876254c7de6edd5ceb63
DanielPaz19 <anyepaz1019@gmail.com>
2022-10-26 00:44:53 +0800

    subject table

--- src/pages/GradeLevels/index.js
+++ src/pages/GradeLevels/index.js
@@ -38,28 +38,72 @@ function StudentTable() {
     </Table>
   );
 }
+function SubjectTable() {
+  return (
+    <Table striped responsive>
+      <thead>
+        <tr className="text-primary">
+          <th>#</th>
+          <th>Subject Code</th>
+          <th>Subject Name</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>00001</td>
+          <td>Mark</td>
+          <td>Mark</td>
+          <td>Otto</td>
+        </tr>
+        <tr>
+          <td>00002</td>
+          <td>Jacob</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+        </tr>
+        <tr>
+          <td>00003</td>
+          <td>Larry the Bird</td>
+          <td>Larry the Bird</td>
+          <td>Larry the Bird</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
 
 function GradeLevels() {
   return (
     <Container fluid="md" className="pt-4">
-      <Tab.Container defaultActiveKey="first">
+      <Tab.Container defaultActiveKey="first" fluid>
         <Row>
-          <Col sm={3} lg={2} className="bg-white py-3 shadow rounded-3">
-            <h4 className="text-primary text-center mb-4">Grade 1</h4>
-            <Nav variant="pills" className="flex-column">
-              <Nav.Item>
-                <Nav.Link eventKey="first">Students</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="second">Subjects</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="third">Topics</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="forth">Quiz</Nav.Link>
-              </Nav.Item>
-            </Nav>
+          <Col sm={3} lg={2}>
+            <Container className="bg-white py-3 px-3 shadow rounded-3">
+              <h4 className="text-primary text-center mb-4">Grade 1</h4>
+              <Nav variant="pills" className="flex-column">
+                <Nav.Item>
+                  <Nav.Link eventKey="first" as="li" className="hover">
+                    Students
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second" as="li" className="hover">
+                    Subjects
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third" as="li" className="hover">
+                    Topics
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="forth" as="li" className="hover">
+                    Quiz
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Container>
           </Col>
           <Col sm={9} lg={10}>
             <Tab.Content>
@@ -67,10 +111,7 @@ function GradeLevels() {
                 <StudentTable />
               </Tab.Pane>
               <Tab.Pane eventKey="second">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non
-                autem porro pariatur inventore magnam quidem atque ullam
-                cupiditate aliquid corporis vitae temporibus sint animi facilis
-                blanditiis ipsam molestiae, nesciunt iusto.
+                <SubjectTable />
               </Tab.Pane>
             </Tab.Content>
           </Col>
