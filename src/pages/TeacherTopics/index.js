8dc7b3a4be3f228aaf7462a05ba04ce5e7065f58
DanielPaz19 <anyepaz1019@gmail.com>
2022-10-27 23:43:45 +0800

    subject card tooltip

--- src/pages/TeacherTopics/index.js
+++ src/pages/TeacherTopics/index.js
@@ -1,9 +1,23 @@
-import { Card, Col, Container, Row } from "react-bootstrap";
+import {
+  Card,
+  Col,
+  Container,
+  OverlayTrigger,
+  Row,
+  Tooltip,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./style.css";
 import useTeacher from "../../context/TeacherContextProvider";
 
 function SubjectCard({ subject }) {
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      {subject.subject_name} <br />
+      {subject.subject_description}
+    </Tooltip>
+  );
+
   return (
     <Col md={3}>
       <Link to="1">
@@ -12,9 +26,15 @@ function SubjectCard({ subject }) {
             <Card.Title className="text-primary fw-bolder text-center">
               {subject.subject_code}
             </Card.Title>
-            <Card.Subtitle className="text-muted text-center text-truncate">
-              {subject.subject_description}
-            </Card.Subtitle>
+            <OverlayTrigger
+              placement="bottom"
+              delay={{ show: 250, hide: 400 }}
+              overlay={renderTooltip}
+            >
+              <Card.Subtitle className="text-muted text-center text-truncate">
+                {subject.subject_name}
+              </Card.Subtitle>
+            </OverlayTrigger>
           </Card.Body>
           <Card.Footer className="text-secondary text-center fs-7">
             Click to View Details
